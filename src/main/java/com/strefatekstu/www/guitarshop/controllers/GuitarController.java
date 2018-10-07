2eb3ef3ff33505f0f573317118b597f9b6bf4afe
kamilnaja <kamilnaja@gmail.com>
2018-10-07 18:18:26 +0200

    added put method

--- src/main/java/com/strefatekstu/www/guitarshop/controllers/GuitarController.java
+++ src/main/java/com/strefatekstu/www/guitarshop/controllers/GuitarController.java
@@ -4,29 +4,44 @@ import com.strefatekstu.www.guitarshop.models.Guitar;
 import com.strefatekstu.www.guitarshop.repositories.GuitarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
-@ResponseBody
 public class GuitarController {
 
     @Autowired
     GuitarRepository guitarRepository;
 
     @ResponseBody
-    @GetMapping("/guitars")
+    @RequestMapping(value = "/guitars", method = RequestMethod.GET)
     public List<Guitar> guitarList() {
         return guitarRepository.findAll();
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/guitars/{id}", method = RequestMethod.GET)
+    public Optional<Guitar> getGuitar(@PathVariable("id") Integer id) {
+        return guitarRepository.findById(id);
+    }
+
     @RequestMapping(value = "/guitars", method = RequestMethod.POST)
-    public String addGuitar(Guitar guitar) {
+    public List<Guitar> addGuitar(Guitar guitar) {
         guitarRepository.save(guitar);
-        return "guitars";
+        return guitarRepository.findAll();
+    }
+
+    @RequestMapping(value = "/guitars/{id}", method = RequestMethod.DELETE)
+    public List<Guitar> removeGuitar(@PathVariable("id") Integer id) {
+        guitarRepository.deleteById(id);
+        return guitarRepository.findAll();
+    }
+
+    @RequestMapping(value = "/guitars/{id}", method = RequestMethod.PUT)
+    public Guitar changeGuitarData(@PathVariable("id") Integer id, @RequestBody Guitar guitar) {
+        guitarRepository.findById(id);
+        return guitarRepository.save(guitar);
     }
 }
