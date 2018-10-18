83ed107a0925a8747e33eef3ae170a69243fc714
= <kamilnaja@gmail.com>
2018-10-18 19:05:51 +0200

    Controller refactor, change db to h2

--- src/main/java/com/strefatekstu/www/guitarshop/controllers/GuitarController.java
+++ src/main/java/com/strefatekstu/www/guitarshop/controllers/GuitarController.java
@@ -3,46 +3,46 @@ package com.strefatekstu.www.guitarshop.controllers;
 import com.strefatekstu.www.guitarshop.models.Guitar;
 import com.strefatekstu.www.guitarshop.repositories.GuitarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @CrossOrigin
-@Controller
+@RestController
 public class GuitarController {
 
-    @Autowired
+    private final
     GuitarRepository guitarRepository;
 
-    @ResponseBody
-    @RequestMapping(value = "/guitars", method = RequestMethod.GET)
+    @Autowired
+    public GuitarController(GuitarRepository guitarRepository) {
+        this.guitarRepository = guitarRepository;
+    }
+
+    @GetMapping("guitars")
     public List<Guitar> guitarList() {
         return guitarRepository.findAll();
     }
 
-    @ResponseBody
-    @RequestMapping(value = "/guitars/{id}", method = RequestMethod.GET)
+    @GetMapping("/guitars/{id}")
     public Optional<Guitar> getGuitar(@PathVariable("id") Integer id) {
         return guitarRepository.findById(id);
     }
 
-    @ResponseBody
-    @RequestMapping(value = "/guitars", method = RequestMethod.POST)
+    @PostMapping("/guitars")
     public List<Guitar> addGuitar(@RequestBody Guitar guitar) {
         guitarRepository.save(guitar);
         return guitarRepository.findAll();
     }
 
-    @ResponseBody
-    @RequestMapping(value = "/guitars/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("/guitars/{id}")
     public List<Guitar> removeGuitar(@PathVariable("id") Integer id) {
         guitarRepository.deleteById(id);
         return guitarRepository.findAll();
     }
 
-    @RequestMapping(value = "/guitars/{id}", method = RequestMethod.PUT)
+    @PutMapping(value = "/guitars/{id}")
     public Guitar changeGuitarData(@PathVariable("id") Integer id, @RequestBody Guitar guitar) {
         guitarRepository.findById(id);
         return guitarRepository.save(guitar);
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into guitars (id, serialnumber, builder, model, type)
-values (1, '001', 'FENDER', 'Telecaster', 'ELECTRIC');
